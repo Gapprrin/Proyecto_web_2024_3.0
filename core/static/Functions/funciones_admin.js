50c0ac4d65875b7fdb6a222abbab759996d1942d
Gapprrin <mate.alvarez@duocuc.cl>
2024-07-12 23:20:00 -0400

    Update 3.5

---
+++ core/static/Functions/funciones_admin.js
@@ -0,0 +1,10 @@
+
+function limpiar(){
+    document.querySelector("#nombre-prod").value = ""
+    document.querySelector("#desc-prod").value = ""
+    document.querySelector("#precio-prod").value = ""
+    document.querySelector("#imagen-accesorio").src = 'static/Images/Admin/accesorio_imagen.png'
+    document.querySelector("#cat-prod").value = ""
+    document.querySelector("#stock-pord").value = ""
+    return
+}
